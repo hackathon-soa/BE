805c2b8413b549a7f9a921328583749f6cfd7c67
galllee <kimim0410@gmail.com>
2025-07-06 03:41:42 +0900

    feat: 지역 기반 검색 api 구현

--- src/main/java/hackathon/soa/domain/home/HomeController.java
+++ src/main/java/hackathon/soa/domain/home/HomeController.java
@@ -6,6 +6,7 @@ import hackathon.soa.domain.home.dto.HomeResponseDTO;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,7 +18,7 @@ public class HomeController {
 
     private final HomeService homeService;
 
-    @PostMapping("/my-courses")
+    @GetMapping("/my-courses")
     @Operation(
             summary = "나의 여행 코스 조회",
             description = "현재 로그인 한 사용자가 생성한 여행 코스를 조회합니다."
@@ -30,7 +31,7 @@ public class HomeController {
         return ApiResponse.onSuccess(result);
     }
 
-    @PostMapping("/my-courses/two")
+    @GetMapping("/my-courses/two")
     @Operation(
             summary = "나의 여행 코스 두개 조회",
             description = "현재 로그인 한 사용자가 생성한 여행 코스 상단 두개를 조회합니다."
